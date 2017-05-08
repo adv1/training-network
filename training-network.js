b2192d353087c9b1b4c9600ede83b09ca928a236
adv1 <andrewlozov@gmail.com>
2017-05-08 14:01:37 +0300

    v 1.4

--- training-network.js
+++ training-network.js
@@ -16,17 +16,12 @@ $(document).ready(function() {
 				data.response.listings.forEach(function(listing, index) {
 					$('.description-house-room')
 						.append('<div class="media">' +
-								  '<div class="pull-left" href="#">' +
-								    '<img class="media-object" src="' +listing.img_url+ ' " alt="' +listing.keywords+ '">' +
+								  '<div class="pull-left">' +
+								    '<img class="media-object" src="' +listing.thumb_url+ ' " alt="' +listing.keywords+ '">' +
 								  '</div>' + 
 								  '<div class="media-body">' + 
 								    '<h4 class="media-heading">' + listing.price_formatted + '</h4>' + 
-								    '<p>' + 'Bathroom number: ' + listing.bathroom_number + ', ' + 
-								    'Bedroom number: ' + listing.bedroom_number + ', ' + '</p>' + 
-								    '<p>' + 'Car spaces: ' + listing.car_spaces + ', ' + '</p>' + 
-								    '<p>' + 'Commission: ' + listing.commission + ', ' + 
-								    'Construction year: ' + listing.construction_year + ', ' + '</p>' + 
-								    '<h3>' + 'Lister name: ' + listing.lister_name + '</h3>' + 
+								    '<h4>' + 'Lister name: ' + listing.lister_name + '</h4>' + 
 								    '<p>' + listing.summary + '</p>' + 
 								  '</div>' + 
 								'</div>');
